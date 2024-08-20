7df1c55b2deb96ee1ef1d4fe0036f6c103083313
kaiky botelho <kaii.bots18@gmail.com>
2024-08-19 22:19:16 -0300

    campos dados2

--- lib/src/pages/profile/profile_tab.dart
+++ lib/src/pages/profile/profile_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nextbook/src/config/custom_colors.dart';
 import 'package:nextbook/src/pages/common_widgets/custom_text_field.dart';
 import 'package:nextbook/src/config/app_data.dart' as appData;
 
@@ -25,42 +26,60 @@ class ProfileTab extends StatelessWidget {
           )
         ],
       ),
-
       body: ListView(
         physics: BouncingScrollPhysics(),
         padding: const EdgeInsets.fromLTRB(16, 32, 16, 16),
         children: [
           //email
           CustomTextField(
-            readOnly: true,
-          initialValue: appData.user.email,
-          icon: Icons.email, 
-          label: 'Email'
-          ),
+              readOnly: true,
+              initialValue: appData.user.email,
+              icon: Icons.email,
+              label: 'Email'),
           //nome
           CustomTextField(
-          readOnly: true,
-          initialValue: appData.user.name,
-          icon: Icons.person, 
-          label: 'Nome'
-          ),
+              readOnly: true,
+              initialValue: appData.user.name,
+              icon: Icons.person,
+              label: 'Nome'),
           //celular
           CustomTextField(
-          readOnly: true,
-          initialValue: appData.user.phone,
-          icon: Icons.phone, 
-          label: 'Celular'
-          ),
+              readOnly: true,
+              initialValue: appData.user.phone,
+              icon: Icons.phone,
+              label: 'Celular'),
           //cpf
-          CustomTextField( 
-          readOnly: true,
-          initialValue: appData.user.cpf,
-          icon: Icons.file_copy, 
-          label: 'CPF',
-          isSecret: true,
+          CustomTextField(
+            readOnly: true,
+            initialValue: appData.user.cpf,
+            icon: Icons.file_copy,
+            label: 'CPF',
+            isSecret: true,
+          ),
+
+          //Botão de atualização de senha
+          SizedBox(
+            height: 50,
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18),
+                ),
+                side: BorderSide(
+                  width: 2,
+                  color: CustomColors.vermelhoNext,
+                ),
+              ),
+              onPressed: () {},
+              child: Text(
+                "Atualizar Senha",
+                style:
+                    TextStyle(color: CustomColors.vermelhoNext, fontSize: 20),
+              ),
+            ),
           ),
         ],
-      ) ,
+      ),
     );
   }
 }
